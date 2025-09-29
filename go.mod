a6695908be1395a6609f7b613ec502036914b806
Amirmohammad Ghasemi <amirmohammad.ghas3mi@gmail.com>
2025-09-29 13:43:02 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module myapp
+
+go 1.21.13
+
+require github.com/xlab/libvpx-go v0.0.0-20220203233824-652b2616315c // indirect
---
+++ sender/main.go
@@ -0,0 +1,404 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+// pion-to-pion is an example of two pion instances communicating directly!
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	vpx "command-line-arguments/home/amirmo/testbed/video/pion/libvpx-go/vpx/types.go"
+
+	"github.com/pion/interceptor"
+	"github.com/pion/interceptor/pkg/cc"
+	"github.com/pion/interceptor/pkg/gcc"
+	"github.com/pion/webrtc/v4"
+	"github.com/pion/webrtc/v4/pkg/media"
+	"github.com/pion/webrtc/v4/pkg/media/ivfreader"
+
+	"github.com/xlab/libvpx-go/vpx"
+)
+
+func signalCandidate(addr string, candidate *webrtc.ICECandidate) error {
+	payload := []byte(candidate.ToJSON().Candidate)
+	resp, err := http.Post( //nolint:noctx
+		fmt.Sprintf("http://%s/candidate", addr),
+		"application/json; charset=utf-8",
+		bytes.NewReader(payload),
+	)
+	if err != nil {
+		return err
+	}
+
+	return resp.Body.Close()
+}
+
+func mustOK(where string, err vpx.CodecErr, ctx *vpx.CodecCtx) {
+	if err != 0 {
+		detail := vpx.CodecErrorDetail(ctx)
+		panic(fmt.Sprintf("%s: %s (%s)", where, vpx.CodecErrToString(err), detail))
+	}
+}
+
+func setup_encoder() (*vpx.CodecIface, vpx.CodecEncCfg) {
+	iface := vpx.EncoderIfaceVP8() // VP8 encoder iface. :contentReference[oaicite:1]{index=1}
+	var cfg vpx.CodecEncCfg
+
+	fpsNum := 1000
+	fpsDen := 24000
+	w := 1920
+	h := 1080
+	bitKbps := 1_000_000
+
+	cfg.GW = w                                             // width
+	cfg.GH = h                                             // height
+	cfg.GTimebase = vpx.Rational{Num: fpsDen, Den: fpsNum} // timebase = 1/fps
+	cfg.RcTargetBitrate = bitKbps
+	// Optional: real-time-ish settings
+	// cfg.GUsage = 1            // usage=real-time (if desired)
+	// cfg.KfMode = 1            // e.g., VPX_KF_AUTO (depends on needs)
+	// cfg.KfMinDist, cfg.KfMaxDist = 0, uint32(fpsNum*10) // keyframe interval
+
+	ctx := vpx.NewCodecCtx()
+	mustOK("CodecEncInitVer",
+		vpx.CodecEncInitVer(ctx, iface, &cfg, 0, vpx.DecoderABIVersion), ctx) // ABI/version params are exported in the pkg. :contentReference[oaicite:3]{index=3}
+
+	return iface, ctx
+}
+
+//nolint:gocognit, cyclop
+func main() {
+
+	setup_encoder()
+
+	offerAddr := flag.String("offer-address", ":50000", "Address that the Offer HTTP server is hosted on.")
+	answerAddr := flag.String("answer-address", "127.0.0.1:60000", "Address that the Answer HTTP server is hosted on.")
+	flag.Parse()
+
+	var candidatesMux sync.Mutex
+	pendingCandidates := make([]*webrtc.ICECandidate, 0)
+
+	iceConnectedCtx, iceConnectedCtxCancel := context.WithCancel(context.Background())
+
+	estimatorChan, peerConnection := setup_peer_connection()
+	defer func() {
+		if cErr := peerConnection.Close(); cErr != nil {
+			fmt.Printf("cannot close peerConnection: %v\n", cErr)
+		}
+	}()
+
+	// When an ICE candidate is available send to the other Pion instance
+	// the other Pion instance will add this candidate by calling AddICECandidate
+	peerConnection.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+		if candidate == nil {
+			return
+		}
+
+		candidatesMux.Lock()
+		defer candidatesMux.Unlock()
+
+		desc := peerConnection.RemoteDescription()
+		if desc == nil {
+			pendingCandidates = append(pendingCandidates, candidate)
+		} else if onICECandidateErr := signalCandidate(*answerAddr, candidate); onICECandidateErr != nil {
+			panic(onICECandidateErr)
+		}
+	})
+
+	// A HTTP handler that allows the other Pion instance to send us ICE candidates
+	// This allows us to add ICE candidates faster, we don't have to wait for STUN or TURN
+	// candidates which may be slower
+	http.HandleFunc("/candidate", func(res http.ResponseWriter, req *http.Request) { //nolint: revive
+		candidate, candidateErr := io.ReadAll(req.Body)
+		if candidateErr != nil {
+			panic(candidateErr)
+		}
+		if candidateErr := peerConnection.AddICECandidate(
+			webrtc.ICECandidateInit{Candidate: string(candidate)},
+		); candidateErr != nil {
+			panic(candidateErr)
+		}
+	})
+
+	// A HTTP handler that processes a SessionDescription given to us from the other Pion process
+	http.HandleFunc("/sdp", func(res http.ResponseWriter, req *http.Request) { //nolint: revive
+		sdp := webrtc.SessionDescription{}
+		if sdpErr := json.NewDecoder(req.Body).Decode(&sdp); sdpErr != nil {
+			panic(sdpErr)
+		}
+
+		if sdpErr := peerConnection.SetRemoteDescription(sdp); sdpErr != nil {
+			panic(sdpErr)
+		}
+
+		candidatesMux.Lock()
+		defer candidatesMux.Unlock()
+
+		for _, c := range pendingCandidates {
+			if onICECandidateErr := signalCandidate(*answerAddr, c); onICECandidateErr != nil {
+				panic(onICECandidateErr)
+			}
+		}
+	})
+	// Start HTTP server that accepts requests from the answer process
+	// nolint: gosec
+	go func() { panic(http.ListenAndServe(*offerAddr, nil)) }()
+
+	estimator := <-estimatorChan
+	handle_video(peerConnection, iceConnectedCtx, estimator)
+
+	// Set the handler for Peer connection state
+	// This will notify you when the peer has connected/disconnected
+	peerConnection.OnConnectionStateChange(func(state webrtc.PeerConnectionState) {
+		fmt.Printf("Peer Connection State has changed: %s\n", state.String())
+
+		if state == webrtc.PeerConnectionStateFailed {
+			// Wait until PeerConnection has had no network activity for 30 seconds or another failure.
+			// It may be reconnected using an ICE Restart.
+			// Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
+			// Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
+			fmt.Println("Peer Connection has gone to failed exiting")
+			os.Exit(0)
+		}
+
+		if state == webrtc.PeerConnectionStateClosed {
+			// PeerConnection was explicitly closed. This usually happens from a DTLS CloseNotify
+			fmt.Println("Peer Connection has gone to closed exiting")
+			os.Exit(0)
+		}
+
+		if state == webrtc.PeerConnectionStateConnected {
+			fmt.Println("Peer Connection has been established")
+			iceConnectedCtxCancel()
+		}
+	})
+
+	// Create an offer to send to the other process
+	offer, err := peerConnection.CreateOffer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	// Note: this will start the gathering of ICE candidates
+	if err = peerConnection.SetLocalDescription(offer); err != nil {
+		panic(err)
+	}
+
+	// Send our offer to the HTTP server listening in the other process
+	payload, err := json.Marshal(offer)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := http.Post( //nolint:noctx
+		fmt.Sprintf("http://%s/sdp", *answerAddr),
+		"application/json; charset=utf-8",
+		bytes.NewReader(payload),
+	)
+	if err != nil {
+		panic(err)
+	} else if err := resp.Body.Close(); err != nil {
+		panic(err)
+	}
+
+	// Block forever
+	select {}
+}
+
+func setup_peer_connection() (chan cc.BandwidthEstimator, *webrtc.PeerConnection) {
+	interceptorRegistry := &interceptor.Registry{}
+	mediaEngine := &webrtc.MediaEngine{}
+	if err := mediaEngine.RegisterDefaultCodecs(); err != nil {
+		panic(err)
+	}
+
+	var lowBitrate = 1_000_000
+	congestionController, err := cc.NewInterceptor(func() (cc.BandwidthEstimator, error) {
+		return gcc.NewSendSideBWE(gcc.SendSideBWEInitialBitrate(lowBitrate))
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	estimatorChan := make(chan cc.BandwidthEstimator, 1)
+	congestionController.OnNewPeerConnection(func(id string, estimator cc.BandwidthEstimator) { //nolint: revive
+		estimatorChan <- estimator
+	})
+
+	interceptorRegistry.Add(congestionController)
+	if err = webrtc.ConfigureTWCCHeaderExtensionSender(mediaEngine, interceptorRegistry); err != nil {
+		panic(err)
+	}
+
+	if err = webrtc.RegisterDefaultInterceptors(mediaEngine, interceptorRegistry); err != nil {
+		panic(err)
+	}
+
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+
+	peerConnection, err := webrtc.NewAPI(
+		webrtc.WithInterceptorRegistry(interceptorRegistry), webrtc.WithMediaEngine(mediaEngine),
+	).NewPeerConnection(config)
+	if err != nil {
+		panic(err)
+	}
+
+	return estimatorChan, peerConnection
+}
+
+const (
+	videoFileName = "input.ivf"
+)
+
+func handle_video(peerConnection *webrtc.PeerConnection, iceConnectedCtx context.Context,
+	estimator cc.BandwidthEstimator) {
+	_, err := os.Stat(videoFileName)
+	haveVideoFile := !os.IsNotExist(err)
+
+	if !haveVideoFile {
+		panic("Could not find `" + videoFileName + "`")
+	}
+
+	file, openErr := os.Open(videoFileName)
+	if openErr != nil {
+		panic(openErr)
+	}
+
+	_, header, openErr := ivfreader.NewWith(file)
+	if openErr != nil {
+		panic(openErr)
+	}
+
+	// Determine video codec
+	var trackCodec string
+	switch header.FourCC {
+	case "AV01":
+		trackCodec = webrtc.MimeTypeAV1
+	case "VP90":
+		trackCodec = webrtc.MimeTypeVP9
+	case "VP80":
+		trackCodec = webrtc.MimeTypeVP8
+	default:
+		panic(fmt.Sprintf("Unable to handle FourCC %s", header.FourCC))
+	}
+
+	// Create a video track
+	videoTrack, videoTrackErr := webrtc.NewTrackLocalStaticSample(
+		webrtc.RTPCodecCapability{MimeType: trackCodec}, "video", "pion",
+	)
+	if videoTrackErr != nil {
+		panic(videoTrackErr)
+	}
+
+	rtpSender, videoTrackErr := peerConnection.AddTrack(videoTrack)
+	if videoTrackErr != nil {
+		panic(videoTrackErr)
+	}
+
+	// Read incoming RTCP packets
+	// Before these packets are returned they are processed by interceptors. For things
+	// like NACK this needs to be called.
+	go func() {
+		rtcpBuf := make([]byte, 1500)
+		for {
+			if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
+				return
+			}
+		}
+	}()
+
+	go func() {
+		// Open a IVF file and start reading using our IVFReader
+		file, ivfErr := os.Open(videoFileName)
+		if ivfErr != nil {
+			panic(ivfErr)
+		}
+
+		ivf, header, ivfErr := ivfreader.NewWith(file)
+		if ivfErr != nil {
+			panic(ivfErr)
+		}
+
+		// Wait for connection established
+
+		<-iceConnectedCtx.Done()
+
+		// Send our video file frame at a time. Pace our sending so we send it at the same speed it should be played back as.
+		// This isn't required since the video is timestamped, but we will such much higher loss if we send all at once.
+		//
+		// It is important to use a time.Ticker instead of time.Sleep because
+		// * avoids accumulating skew, just calling time.Sleep didn't compensate for the time spent parsing the data
+		// * works around latency issues with Sleep (see https://github.com/golang/go/issues/44343)
+
+		fmt.Printf("IVF Header Information:\n")
+		fmt.Printf("FourCC: %s\n", header.FourCC)
+		fmt.Printf("Width: %d\n", header.Width)
+		fmt.Printf("Height: %d\n", header.Height)
+		fmt.Printf("Timebase Numerator: %d\n", header.TimebaseNumerator)
+		fmt.Printf("Timebase Denominator: %d\n", header.TimebaseDenominator)
+		fmt.Printf("Number of Frames: %d\n", header.NumFrames)
+
+		duration := time.Millisecond * time.Duration((float32(header.TimebaseNumerator)/float32(header.TimebaseDenominator))*1000)
+		ticker := time.NewTicker(duration)
+
+		defer ticker.Stop()
+		var cnt = 0
+		for ; true; <-ticker.C {
+
+			targetBitrate := estimator.GetTargetBitrate()
+			fmt.Println("target bitrate is ", targetBitrate)
+
+			frame, header, ivfErr := ivf.ParseNextFrame()
+			if errors.Is(ivfErr, io.EOF) {
+				fmt.Printf("All video frames parsed and sent")
+				peerConnection.Close()
+				os.Exit(0)
+			}
+
+			if ivfErr != nil {
+				panic(ivfErr)
+			}
+
+			// for debugging video
+			{
+				cnt += 1
+				hash := sha256.Sum256(frame)
+				hashStr := base64.StdEncoding.EncodeToString(hash[:])
+
+				// headerBytes := make([]byte, 12)
+				headerBytes := new(bytes.Buffer)
+				if err := binary.Write(headerBytes, binary.BigEndian, header); err != nil {
+					panic(err)
+				}
+				headerHash := sha256.Sum256(headerBytes.Bytes())
+				headerHashStr := base64.StdEncoding.EncodeToString(headerHash[:])
+
+				fmt.Println("frame ", cnt, " with frame size ", header.FrameSize, "with timestamp", header.Timestamp)
+				fmt.Println("Sending frame ", cnt, " with header hash", headerHashStr, " with hash ", hashStr)
+			}
+
+			if ivfErr = videoTrack.WriteSample(media.Sample{Data: frame, Duration: 24 * time.Millisecond}); ivfErr != nil {
+				panic(ivfErr)
+			}
+		}
+	}()
+}
