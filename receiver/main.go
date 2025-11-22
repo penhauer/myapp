7401525e54c482de57bc37994af915cfbacbd167
Amirmohammad Ghasemi <amirmohammad.ghas3mi@gmail.com>
2025-11-22 16:36:30 -0500

    Refactor receiver

--- receiver/main.go
+++ receiver/main.go
@@ -5,15 +5,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/pion/interceptor"
@@ -24,37 +21,15 @@ import (
 	"github.com/pion/webrtc/v4/pkg/media/h265writer"
 )
 
-func signalCandidate(addr string, candidate *webrtc.ICECandidate) error {
-	payload := []byte(candidate.ToJSON().Candidate)
-	resp, err := http.Post(fmt.Sprintf("http://%s/candidate", addr), // nolint:noctx
-		"application/json; charset=utf-8", bytes.NewReader(payload))
-	if err != nil {
-		return err
-	}
+var logger logging.LeveledLogger = setup_logger()
 
-	return resp.Body.Close()
-}
-
-// nolint:gocognit, cyclop
 func main() {
 	offerAddr := flag.String("offer-address", "localhost:50000", "Address that the Offer HTTP server is hosted on.")
 	answerAddr := flag.String("answer-address", "0.0.0.0:60000", "Address that the Answer HTTP server is hosted on.")
 	flag.Parse()
 
-	var err error
-	interceptorRegistry := &interceptor.Registry{}
-	mediaEngine := &webrtc.MediaEngine{}
-	if err := mediaEngine.RegisterDefaultCodecs(); err != nil {
-		panic(err)
-	}
-
-	webrtc.ConfigureCongestionControlFeedback(mediaEngine, interceptorRegistry)
-
-	// if err = webrtc.ConfigureTWCCHeaderExtensionSender(mediaEngine, interceptorRegistry); err != nil {
-	// 	panic(err)
-	// }
-
-	if err = webrtc.RegisterDefaultInterceptors(mediaEngine, interceptorRegistry); err != nil {
+	mediaEngine, interceptorRegistry, err := registerInterceptors()
+	if err != nil {
 		panic(err)
 	}
 
@@ -65,9 +40,9 @@ func main() {
 			},
 		},
 	}
-
 	peerConnection, err := webrtc.NewAPI(
-		webrtc.WithInterceptorRegistry(interceptorRegistry), webrtc.WithMediaEngine(mediaEngine),
+		webrtc.WithInterceptorRegistry(interceptorRegistry),
+		webrtc.WithMediaEngine(mediaEngine),
 	).NewPeerConnection(config)
 	if err != nil {
 		panic(err)
@@ -79,121 +54,38 @@ func main() {
 		}
 	}()
 
-	var candidatesMux sync.Mutex
-	pendingCandidates := make([]*webrtc.ICECandidate, 0)
-
-	// When an ICE candidate is available send to the other Pion instance
-	// the other Pion instance will add this candidate by calling AddICECandidate
-	peerConnection.OnICECandidate(func(candidate *webrtc.ICECandidate) {
-		if candidate == nil {
-			return
-		}
-
-		candidatesMux.Lock()
-		defer candidatesMux.Unlock()
-
-		desc := peerConnection.RemoteDescription()
-		if desc == nil {
-			pendingCandidates = append(pendingCandidates, candidate)
-		} else if onICECandidateErr := signalCandidate(*offerAddr, candidate); onICECandidateErr != nil {
-			panic(onICECandidateErr)
-		}
-	})
-
-	// A HTTP handler that allows the other Pion instance to send us ICE candidates
-	// This allows us to add ICE candidates faster, we don't have to wait for STUN or TURN
-	// candidates which may be slower
-	http.HandleFunc("/candidate", func(res http.ResponseWriter, req *http.Request) { //nolint: revive
-		candidate, candidateErr := io.ReadAll(req.Body)
-		if candidateErr != nil {
-			panic(candidateErr)
-		}
-		if candidateErr := peerConnection.AddICECandidate(
-			webrtc.ICECandidateInit{Candidate: string(candidate)},
-		); candidateErr != nil {
-			panic(candidateErr)
-		}
-	})
-
-	// A HTTP handler that processes a SessionDescription given to us from the other Pion process
-	http.HandleFunc("/sdp", func(res http.ResponseWriter, req *http.Request) { // nolint: revive
-		sdp := webrtc.SessionDescription{}
-		if err := json.NewDecoder(req.Body).Decode(&sdp); err != nil {
-			panic(err)
-		}
-
-		if err := peerConnection.SetRemoteDescription(sdp); err != nil {
-			panic(err)
-		}
-
-		// Create an answer to send to the other process
-		answer, err := peerConnection.CreateAnswer(nil)
-		if err != nil {
-			panic(err)
-		}
-
-		// Send our answer to the HTTP server listening in the other process
-		payload, err := json.Marshal(answer)
-		if err != nil {
-			panic(err)
-		}
-		resp, err := http.Post( //nolint:noctx
-			fmt.Sprintf("http://%s/sdp", *offerAddr),
-			"application/json; charset=utf-8",
-			bytes.NewReader(payload),
-		) // nolint:noctx
-		if err != nil {
-			panic(err)
-		} else if closeErr := resp.Body.Close(); closeErr != nil {
-			panic(closeErr)
-		}
-
-		// Sets the LocalDescription, and starts our UDP listeners
-		err = peerConnection.SetLocalDescription(answer)
-		if err != nil {
-			panic(err)
-		}
-
-		candidatesMux.Lock()
-		for _, c := range pendingCandidates {
-			onICECandidateErr := signalCandidate(*offerAddr, c)
-			if onICECandidateErr != nil {
-				panic(onICECandidateErr)
-			}
-		}
-		candidatesMux.Unlock()
-	})
-
-	// Set the handler for Peer connection state
-	// This will notify you when the peer has connected/disconnected
-	peerConnection.OnConnectionStateChange(func(state webrtc.PeerConnectionState) {
-		fmt.Printf("Peer Connection State has changed: %s\n", state.String())
-
-		if state == webrtc.PeerConnectionStateFailed {
-			// Wait until PeerConnection has had no network activity for 30 seconds or another failure.
-			// It may be reconnected using an ICE Restart.
-			// Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
-			// Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
-			fmt.Println("Peer Connection has gone to failed exiting")
-			os.Exit(0)
-		}
-
-		if state == webrtc.PeerConnectionStateClosed {
-			// PeerConnection was explicitly closed. This usually happens from a DTLS CloseNotify
-			fmt.Println("Peer Connection has gone to closed exiting")
-			os.Exit(0)
-		}
-	})
+	// register signaling, ICE and connection-state handlers (moved to util.go)
+	registerSignalingHandlers(peerConnection, offerAddr)
 
 	handle_video(peerConnection)
 
 	// Start HTTP server that accepts requests from the offer process to exchange SDP and Candidates
-	// nolint: gosec
 	panic(http.ListenAndServe(*answerAddr, nil))
 }
 
+func registerInterceptors() (*webrtc.MediaEngine, *interceptor.Registry, error) {
+	interceptorRegistry := &interceptor.Registry{}
+	mediaEngine := &webrtc.MediaEngine{}
+	if err := mediaEngine.RegisterDefaultCodecs(); err != nil {
+		return nil, nil, err
+	}
+
+	if err := webrtc.ConfigureCongestionControlFeedback(mediaEngine, interceptorRegistry); err != nil {
+		return nil, nil, err
+	}
+
+	return mediaEngine, interceptorRegistry, nil
+}
+
+func setup_logger() logging.LeveledLogger {
+	loggerFactory := logging.NewDefaultLoggerFactory()
+	loggerFactory.DefaultLogLevel.Set(logging.LogLevelDebug)
+	logger := loggerFactory.NewLogger("receiver")
+	return logger
+}
+
 func handle_video(peerConnection *webrtc.PeerConnection) {
-	outputFile := getExperimentDirEnvVar() + "/received.h265"
+	outputFile := filepath.Join(getExperimentDirEnvVar(), "/received.h265")
 	writer, err := h265writer.New(outputFile)
 	if err != nil {
 		panic(err)
@@ -202,7 +94,7 @@ func handle_video(peerConnection *webrtc.PeerConnection) {
 	peerConnection.OnTrack(func(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) { //nolint: revive
 		codec := track.Codec()
 		if strings.EqualFold(codec.MimeType, webrtc.MimeTypeH265) {
-			fmt.Println("Got H265 track, saving to disk as recevied.265")
+			logger.Info("Got H265 track, saving to disk as recevied.265")
 			saveToDisk(writer, track)
 		}
 	})
@@ -212,8 +104,8 @@ func handle_video(peerConnection *webrtc.PeerConnection) {
 func getExperimentDirEnvVar() string {
 	dir := os.Getenv("EXPERIMENT_DIR")
 	if dir == "" {
+		logger.Warn("required environment variable EXPERIMENT_DIR is not set")
 		dir = "."
-		// log.Fatalf("required environment variable EXPERIMENT_DIR is not set")
 	}
 	return dir
 }
@@ -225,10 +117,6 @@ func saveToDisk(writer media.Writer, track *webrtc.TrackRemote) {
 		}
 	}()
 
-	loggerFactory := logging.NewDefaultLoggerFactory()
-	fmt.Println(loggerFactory.DefaultLogLevel.Get())
-	loggerFactory.DefaultLogLevel.Set(logging.LogLevelInfo)
-	logger := loggerFactory.NewLogger("receiver")
 	logger.Debugf("Start reception at %s\n", time.Now().Format(time.StampMilli))
 
 	frameCnt := 0
---
+++ receiver/util.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/pion/webrtc/v4"
+)
+
+// signalCandidate posts a single ICE candidate to the remote HTTP server.
+func signalCandidate(addr string, candidate *webrtc.ICECandidate) error {
+	payload := []byte(candidate.ToJSON().Candidate)
+
+	resp, err := http.Post(fmt.Sprintf("http://%s/candidate", addr), // nolint:noctx
+		"application/json; charset=utf-8", bytes.NewReader(payload))
+	if err != nil {
+		return err
+	}
+
+	return resp.Body.Close()
+}
+
+// registerSignalingHandlers wires up ICE candidate handling, SDP exchange and connection-state handling.
+// It registers HTTP handlers for /candidate and /sdp but does not start the HTTP server.
+func registerSignalingHandlers(peerConnection *webrtc.PeerConnection, offerAddr *string) {
+
+	var candidatesMux sync.Mutex
+	pendingCandidates := make([]*webrtc.ICECandidate, 0)
+
+	// When an ICE candidate is available send to the other Pion instance
+	peerConnection.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+		if candidate == nil {
+			return
+		}
+
+		candidatesMux.Lock()
+		defer candidatesMux.Unlock()
+
+		desc := peerConnection.RemoteDescription()
+		if desc == nil {
+			pendingCandidates = append(pendingCandidates, candidate)
+		} else if err := signalCandidate(*offerAddr, candidate); err != nil {
+			panic(err)
+		}
+	})
+
+	// HTTP handler to receive ICE candidates from remote
+	http.HandleFunc("/candidate", func(res http.ResponseWriter, req *http.Request) { //nolint: revive
+		candidate, candidateErr := io.ReadAll(req.Body)
+		if candidateErr != nil {
+			panic(candidateErr)
+		}
+		if candidateErr := peerConnection.AddICECandidate(
+			webrtc.ICECandidateInit{Candidate: string(candidate)},
+		); candidateErr != nil {
+			panic(candidateErr)
+		}
+	})
+
+	// HTTP handler to process remote SDP and send our answer
+	http.HandleFunc("/sdp", func(res http.ResponseWriter, req *http.Request) { // nolint: revive
+		sdp := webrtc.SessionDescription{}
+		if err := json.NewDecoder(req.Body).Decode(&sdp); err != nil {
+			panic(err)
+		}
+
+		if err := peerConnection.SetRemoteDescription(sdp); err != nil {
+			panic(err)
+		}
+
+		// Create an answer to send to the other process
+		answer, err := peerConnection.CreateAnswer(nil)
+		if err != nil {
+			panic(err)
+		}
+
+		// Send our answer to the HTTP server listening in the other process
+		payload, err := json.Marshal(answer)
+		if err != nil {
+			panic(err)
+		}
+		resp, err := http.Post( //nolint:noctx
+			fmt.Sprintf("http://%s/sdp", *offerAddr),
+			"application/json; charset=utf-8",
+			bytes.NewReader(payload),
+		) // nolint:noctx
+		if err != nil {
+			panic(err)
+		} else if closeErr := resp.Body.Close(); closeErr != nil {
+			panic(closeErr)
+		}
+
+		// Sets the LocalDescription, and starts our UDP listeners
+		err = peerConnection.SetLocalDescription(answer)
+		if err != nil {
+			panic(err)
+		}
+
+		candidatesMux.Lock()
+		for _, c := range pendingCandidates {
+			if onICECandidateErr := signalCandidate(*offerAddr, c); onICECandidateErr != nil {
+				panic(onICECandidateErr)
+			}
+		}
+		candidatesMux.Unlock()
+	})
+
+	// Connection state handler
+	peerConnection.OnConnectionStateChange(func(state webrtc.PeerConnectionState) {
+		fmt.Printf("Peer Connection State has changed: %s\n", state.String())
+
+		if state == webrtc.PeerConnectionStateFailed {
+			fmt.Println("Peer Connection has gone to failed exiting")
+			os.Exit(0)
+		}
+
+		if state == webrtc.PeerConnectionStateClosed {
+			fmt.Println("Peer Connection has gone to closed exiting")
+			os.Exit(0)
+		}
+	})
+}
